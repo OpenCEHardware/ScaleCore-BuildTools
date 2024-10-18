018b322f6bbaba4835ff72155d42cc8fb37d0bc3
Alejandro Soto <alejandro@34project.org>
2024-10-17 19:48:17 -0600

    mk/cc: enable building cc targets

--- mk/cc.mk
+++ mk/cc.mk
@@ -1,3 +1,5 @@
+targets += cc
+
 cc_srcs = $(call core_paths,$(1),cc_files)
 ld_objs = $(call cc_srcs_to_objs,$(1),$(call cc_srcs,$(1))) $(call core_objs,$(1),ld_extra)
 cc_srcs_to_objs = $(addsuffix .o,$(addprefix $(obj)/cc/$(1)/,$(basename $(notdir $(2)))))
@@ -27,10 +29,25 @@ define cc_unit_rule
   define obj_rules
     $(4): | $$(obj)/cc/$(2)
     $(4): $(3) $$(obj_deps)
-		$$(call run,CC,$$<) $(core_info/$(1)/cross)gcc $(core_info/$(1)/cc_flags) -MMD -c $$< -o $$@
+		$$(call run,CC,$$<) $(core_info/$(1)/cross)gcc $(core_info/$(1)/cc_flags) $(if $(enable_opt),-O3 -ffast-math) -MMD -c $$< -o $$@
   endef
 
   $$(eval $$(call add_obj_rules,$(1)))
 
   -include $$(patsubst %.o,%.d,$(4))
+endef
+
+define target/cc/prepare
+endef
+
+define target/cc/setup
+  $$(eval $$(call build_vars,enable_opt))
+endef
+
+define target/cc/rules
+  .PHONY: $$(rule_top_path)/cc
+
+  $$(rule_top_path)/cc: $$(call require_core_objs,$(rule_top),ld_binary)
+
+  $(call target_entrypoint,$$(rule_top_path)/cc)
 endef
